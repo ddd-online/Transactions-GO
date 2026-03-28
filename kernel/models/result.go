44925485d6cbf20a913d9d91d23bd72b86cbb780
ljw <liujingwei1997@outlook.com>
2026-03-29 01:11:52 +0800

    format

--- kernel/models/result.go
+++ kernel/models/result.go
@@ -1,9 +1,9 @@
 package models
 
 type Result struct {
-	Code int         `json:"code"`           // return code
-	Msg  string      `json:"msg,omitempty"`  // message
-	Data interface{} `json:"data,omitempty"` // data object
+	Code int    `json:"code"`           // return code
+	Msg  string `json:"msg,omitempty"`  // message
+	Data any    `json:"data,omitempty"` // data object
 }
 
 // NewResult creates a result with Code=0, Msg="", Data=nil.
